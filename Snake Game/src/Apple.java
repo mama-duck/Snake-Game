6f945b18a5ef522e1cb50313d83bbdb47f78974e
Mr-Mitchell-SOCS <120413933+Mr-Mitchell-SOCS@users.noreply.github.com>
2022-12-16 10:30:54 -0500

    Apple Comments

--- Snake Game/src/Apple.java
+++ Snake Game/src/Apple.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 
 public class Apple {
+	//Same as snake class: attributes private with methods to access
 	public int posX, posY, width, height;
 	public Color color;
 	public Apple(int posX, int posY, int width, int height, Color c) {
